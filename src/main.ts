e8fc758b50e1e43bd1e7cc1580813b5ea8bcca88
oleksii <oleksiizhuk.att@gmail.com>
2021-05-06 22:04:33 +0300

    add gitignore

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
+
   const app = await NestFactory.create(AppModule);
 
   app.useGlobalPipes(new ValidationPipe());
