b14cf9408b4d603d1c666fce67a4acc6593b0676
OleksiiZhuk <oleksiizhuk.att@gmail.com>
2021-08-25 20:47:31 +0300

    refactor

--- src/components/user/user.module.ts
+++ src/components/user/user.module.ts
@@ -1,14 +1,11 @@
-import { Module } from '@nestjs/common';
-import { UserService } from './user.service';
-import { UserController } from './user.controller';
-import { UserSchema } from './schema/user.schema';
-import { MongooseModule } from '@nestjs/mongoose';
-
+import { Module } from "@nestjs/common";
+import { UserService } from "./user.service";
+import { UserController } from "./user.controller";
+import { UserSchema } from "./schema/user.schema";
+import { MongooseModule } from "@nestjs/mongoose";
 
 @Module({
-  imports: [
-    MongooseModule.forFeature([{ name: 'User', schema: UserSchema }]),
-  ],
+  imports: [MongooseModule.forFeature([{ name: "User", schema: UserSchema }])],
   providers: [UserService],
   controllers: [UserController],
   exports: [UserService],
